04f27526bdaf539ad6edffb3ae7c4f76a3f76183
Karameule <victor.robidet@hotmail.com>
2023-10-18 16:27:52 +0200

    Création Carte Produits avec érreur

---
+++ app/account/components/productInfo.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Logo from '../images/rocket-game.jpg'
+import { table } from 'console';
+
+type Product = {
+    id: number,
+    title: string,
+    description: string,
+    platform: string,
+    productCondition: string,
+}
+
+export default function ProductInfo(product: Product) {
+    const [productInfo, setProductInfo] = useState({
+        title: "Jeux Rocket League",
+        description: "Je vends ce jeu car je n'y joue plus.",
+        productCondition: "MINT",
+        platform: "XBOX",
+    });
+
+    useEffect(() => {
+        //COnnexion
+        console.log(product);
+
+    }, []);
+
+    return (
+        <div className="overflow-x-auto">
+            <table className="min-w-full">
+                <div className="bg-white p-4 rounded-lg shadow-md mt-2 flex">
+                    <figure>
+                        <Image
+                            src={Logo}
+                            className="w-36 h-36 object-cover flex"
+                            alt="jeu"
+                        />
+                    </figure>
+                    <div className="p-4">
+                        <h2 className="text-2xl font-semibold mb-2">{productInfo.title}</h2>
+                        <div className="mb-2">
+                            <strong>État du produit:</strong> {productInfo.productCondition}
+                        </div>
+                        <div className="mb-2">
+                            <strong>Plateforme:</strong> {productInfo.platform}
+                        </div>
+                        <p>{productInfo.description}</p>
+                    </div>
+                </div>
+            </table>
+        </div>
+
+    )
+}
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,8 +1,28 @@
-export default function Account() {
+"use client"
+import React from 'react';
+import ProfileInfo from './components/profileInfo';
+import AddressInfo from './components/addressInfo';
+import ProductInfo from './components/productInfo';
+import TableProduct from './components/tableProduct';
 
+export default function Account() {
     return (
-        <main>
-            Page account
-        </main>
+
+        <div className="container mx-auto p-4">
+            <div className="flex items-center space-x-2">
+                <div className="avatar online placeholder">
+                    <div className="bg-neutral-focus text-neutral-content rounded-full w-16 ">
+                        <span className="text-xl">JO</span>
+                    </div>
+                </div>
+                <h1 className="text-2xl font-semibold mb-4 mt-5"> Username </h1>
+            </div>
+
+            <ProfileInfo />
+            <AddressInfo />
+            <h2 className="text-2xl font-semibold mb-2 mt-4 text-center"> Articles mis en ventes</h2>
+            <TableProduct />
+
+        </div>
     );
 }
